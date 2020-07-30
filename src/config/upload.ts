605648024e2ca6f4909431cb37a42335a1a9a2b8
Thiago Mendes <thiagoromendes@gmail.com>
2020-07-30 11:46:05 -0300

    Refatoração da estrutura do projeto

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -5,15 +5,15 @@ import multer from 'multer';
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-    directory: tmpFolder,
+  directory: tmpFolder,
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString("hex");
-            const fileName = `${fileHash}-${file.originalname}`;
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
 
-            return callback(null, fileName);
-        }
-    })
-}
+      return callback(null, fileName);
+    },
+  }),
+};
