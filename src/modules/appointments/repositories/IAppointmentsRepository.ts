76ad2cf7bb90140a6150c243f79f1d525af98d30
Thiago Mendes <thiagoromendes@gmail.com>
2020-08-11 17:43:43 -0300

    Implementado inicial de service e testes unitários para listagem de dias disponíveis para agendamento

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   findByDate(date: Date): Promise<Appointment | undefined>;
   create(data: ICreateAppointmentDTO): Promise<Appointment | undefined>;
+  findAllInMothFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
