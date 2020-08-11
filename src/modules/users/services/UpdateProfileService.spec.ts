8d9da5f82b671942238ad617099b5ef2fe7ccb0c
Thiago Mendes <thiagoromendes@gmail.com>
2020-08-11 14:25:55 -0300

    Conclusão da atualização de perfil de usuário, seus testes unitários e rotas

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -109,4 +109,14 @@ describe('UpdateProfile', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should not be able update the profile from non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@test.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
