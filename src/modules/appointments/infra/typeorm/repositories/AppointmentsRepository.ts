bb8d705051ebae17e9cf03fc5f1d6d029b952c4e
Thiago Mendes <thiagoromendes@gmail.com>
2020-09-02 21:06:51 -0300

    Ajuste em repositories e services de appointments

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -30,6 +30,7 @@ class AppoitmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName},'DD-MM-YYYY') = '${parseDay}-${parseMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
     return appointments;
   }
